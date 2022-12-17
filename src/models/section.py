f0767c0d401fd29943fef0d70a1674c8afe71ddf
Cole-Harp <coleharp@hotmail.com>
2022-12-17 12:51:14 -0600

    Sections, maybe should be in Classes?

---
+++ src/models/section.py
@@ -0,0 +1,8 @@
+
+class Sections:
+
+    def __init__(self, class_name, section_id, professor, num_students):
+        self.class_name = class_name
+        self.section_id = section_id
+        self.professor = professor
+        self.num_students = num_students
