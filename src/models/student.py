7d25d7cff83715aed76674957f1474b11a36d498
Cole-Harp <coleharp@hotmail.com>
2022-12-22 14:30:40 -0600

    Additions based off of "Placing entering students into CIS courses (CIS 115 vs. CIS 121)"

--- src/models/student.py
+++ src/models/student.py
@@ -1,5 +1,5 @@
 class Student:
-    def __init__(self, student_id, course_history, dfw_rate):
+    def __init__(self, student_id: str, course_history, dfw_rate: float, programming_experience: bool):
         # I doubt we are getting names, so I figure some type of identifier is
         # necessary?
         self.student_id = student_id
@@ -8,7 +8,9 @@ class Student:
         self.course_history: list[tuple[str, str, int]] = course_history
         # percentage of students with grade <= D, maybe this should be a
         # variable in class.
-        self.dfw_rate
+        self.dfw_rate = dfw_rate
+        self.programming_experience = programming_experience
+        self.precalc = None  # has_precalc():
         self.gpa = None  # get_gpa()
 
     def get_gpa(self) -> float:
@@ -32,3 +34,20 @@ class Student:
         gpa = grade_points / len(self.course_history)
         self.gpa = gpa
         return gpa
+
+    def get_start_class(self) -> bool:
+        """Decide whether student should start in CIS115 or CIS121"""
+        if self.programming_experience:
+            # TODO: check if enrolled class is MATH 115 or ahead. If true student can take CIS 121, else can
+            # if math enrollment >= MATH115: return True
+            # else: return False
+        else:
+            return False
+
+    #TODO
+    def has_precalc(self):
+        pass
+
+    #TODO
+    def highest_grade(self) -> str:
+        pass
