34637660b13d7b878e61f75692b06474815c8a84
Braxton Fair <hello@taxborn.com>
2022-12-29 12:00:57 -0600

    Add parsing student data

--- src/main.py
+++ src/main.py
@@ -1,5 +1,24 @@
+import json
+import models.student as stud
+
+
 def main():
-    print("Hello, world")
+    student_data = json.load(open("students.json"))
+    students = []
+
+    for student in student_data["students"]:
+        student_id = student["id"]
+        courses = []
+
+        for course in student["courses"]:
+            courses.append(
+                (course["name"], course["grade"], course["credits"]))
+
+        std = stud.Student(student_id, courses)
+        students.append(std)
+
+    for student in students:
+        print(student.gpa)
 
 
 if __name__ == "__main__":
