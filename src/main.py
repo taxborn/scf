5d2e5f9983305411592c5ef992e7e93f9df4adc2
Cole-Harp <coleharp@hotmail.com>
2022-12-30 11:14:00 -0600

    Sequencing stuff

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import json
 from models.student import Student
+from models.course import Course
 
 
 def main():
@@ -40,6 +41,29 @@ def get_list_of_students(path: str) -> list[Student]:
 
     return students
 
+def main2():
+    CIS115 = Course("CIS-115", 4, "C", False)
+    CIS121 = Course("CIS-121", 4, "C", False)
+    CIS122 = Course("CIS-122", 4, "C", False)
+    CIS223 = Course("CIS-223", 4, "C", False)
+    CIS224 = Course("CIS-224", 4, "C", False)
+    MATH121 = Course("MATH-121", 4, "C", True)
+
+    CIS121.create_course_sequence([CIS115, CIS121, CIS122, CIS223, CIS224])
+
+    CIS121.add_prereq(MATH121)
+
+    print(CIS121.check_prereqs())
+
+    print(CIS121.set_prereq_given_course_sequence())
+
+    print(CIS121.set_prereq_for_given_course_sequence())
+
+
+
+
+
+
 
 if __name__ == "__main__":
-    main()
+    main2()
--- src/models/course.py
+++ src/models/course.py
@@ -7,6 +7,10 @@ class Course:
         self.min_grade = min_grade
         # A way to differentiate
         self.is_math = self.is_math_course(is_math)
+        # Course sequencing
+        self.course_sequence = None
+        self.prereqs: list[Course] = []
+        self.prereq_for = []
 
     def is_math_course(self, is_math: bool | None = None) -> bool:
         """
@@ -24,3 +28,30 @@ class Course:
 
         # Otherwise, return False
         return False
+
+    def create_course_sequence(self, courses: list):
+        self.course_sequence = courses
+    def add_prereq(self, course):
+        self.prereqs.append(course)
+
+    def check_prereqs(self):
+        return [course for course in self.prereqs]
+
+    def set_prereq_given_course_sequence(self):
+        for i in range(len(self.course_sequence)):
+            if self.course_sequence[i].course_name == self.course_name:
+                self.prereqs.append(self.course_sequence[i-1])
+                break
+        return self.prereqs
+
+    def add_prereq_for(self, course):
+        self.prereq_for.append(course)
+    def is_prereq_for(self):
+        return [course for course in self.prereq_for]
+
+    def set_prereq_for_given_course_sequence(self):
+        for i in range(len(self.course_sequence)):
+            if self.course_sequence[i].course_name == self.course_name:
+                self.prereq_for.append(self.course_sequence[i+1])
+                break
+        return self.prereq_for
