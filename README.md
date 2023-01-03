b8ba7aa87206e104a24b8cf6c213b2dccee5928d
Braxton Fair <hello@taxborn.com>
2023-01-02 20:41:53 -0600

    Update output to add major

--- README.md
+++ README.md
@@ -1,17 +1,24 @@
 # Winter project
 Winter 2022 project. Utilizes [Python](https://www.python.org/) and
-[Pip](https://pypi.org/project/pip/). This currently utilizes test data
-in [students.json](./students.json), which is generated programatically from 
-[student_generator.py](./student_generator.py). In there you will find certain 
-parameters that you can change to simulate different data.
+[Pip](https://pypi.org/project/pip/). 
 
 # Running the code
-Currently the only requirements are [pytest](https://docs.pytest.org/en/7.2.x/)
+Currently the only requirements are [pytest](https://docs.pytest.org/en/7.2.x/),
+which can be installed with the command:
+```
+pip install -r requirements.txt
+```
 
 Other than that, just run the main script:
 ```
 python src/main.py
 ```
+
+# Generating student data
+Student data is generated in a configurable python script, located at [./student_generator.py](./student_generator.py). Running that generates `student-{cs, cit, mis, hi}.json` files, each with students of the respective major.
+
+# Generating CSV of student data
+CSV generation is done in another helper script, located at [./json-to-csv.py](./json-to-csv.py). Running that will look for the `student-{cs, cit, mis, hi}.json` files with student data, and compile to one `compiled_students.csv` file.
 
 # Tests
 The test suite utilizes [pytest](https://docs.pytest.org/en/7.2.x/). To execute
--- json-to-csv.py
+++ json-to-csv.py
@@ -1,5 +1,6 @@
 # Python program to convert
 # JSON file to CSV
+import os
 import json
 import csv
 
@@ -19,16 +20,23 @@ cit_students = cit_data['students']
 mis_students = mis_data['students']
 hi_students = hi_data['students']
 
+filename = "compiled_students.csv"
+
+# Remove the file if it exists
+if os.path.exists(filename):
+    os.remove(filename)
+
 # now we will open a file for writing
-data_file = open('data_file.csv', 'w')
+data_file = open(filename, "w")
 
 # create the csv writer object
 csv_writer = csv.writer(data_file)
 
 
-def print_student(student):
+def print_student(major, student):
     output = []
     output.append(student["id"])
+    output.append(major)
     output.append(student["programming_experience"])
 
     # Writing data of CSV
@@ -41,25 +49,22 @@ def print_student(student):
 # Counter variable used for writing
 # headers to the CSV file
 count = 0
+header = ["Student ID", "Major", "Programming Experience?",
+          "Courses (Course name, Grade Achieved)"]
+csv_writer.writerow(header)
 
 for student in cs_students:
-    if count == 0:
-        # Writing headers of CSV file
-        header = student.keys()
-        csv_writer.writerow(header)
-        count += 1
-
     # Writing data of CSV
-    print_student(student)
+    print_student("CS", student)
 
 for student in cit_students:
-    print_student(student)
+    print_student("CIT", student)
 
 for student in mis_students:
-    print_student(student)
+    print_student("MIS", student)
 
 for student in hi_students:
-    print_student(student)
+    print_student("HI", student)
 
 
 data_file.close()
